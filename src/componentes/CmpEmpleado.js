d63766276bd9f7d8c0ddc6b7a598396b86d630e7
Daniel Tarqui <28693068+dtarqui@users.noreply.github.com>
2025-03-14 09:54:24 -0400

    ejercicio cambio de estado

---
+++ src/componentes/CmpEmpleado.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "../App.css";
+function CmpEmpleado({ empleado, cambiarEstadoEmpleado }) {
+  return (
+    <div className={`empleado-card ${empleado.activo ? "activo" : "inactivo"}`}>
+      <p>
+        <strong>
+          {empleado.apellido} {empleado.nombre}
+        </strong>
+      </p>
+      <p>Cargo: {empleado.cargo}</p>
+      <p>
+        Estado:{" "}
+        <span className={empleado.activo ? "estado-activo" : "estadoinactivo"}>
+          {empleado.activo ? "Activo " : "Inactivo "}
+        </span>
+      </p>
+      <button
+        className="cambiar-estado-btn"
+        onClick={() => cambiarEstadoEmpleado(empleado.id)}
+      >
+        Cambiar Estado
+      </button>
+    </div>
+  );
+}
+export default CmpEmpleado;
